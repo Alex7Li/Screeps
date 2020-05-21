d71592ddcf903c685d148cec7e3eb868a5123cef
Alex <7alex7li@gmail.com>
2020-05-21 12:23:01 -0400

    builder role

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let roleHarvester = require('role.harvester');
 let roleUpgrader = require('role.upgrader');
+let roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
     for(let name in Game.creeps) {
@@ -9,6 +10,9 @@ module.exports.loop = function () {
         }
         if(creep.memory.role === 'upgrader') {
             roleUpgrader.run(creep);
+        }
+        if(creep.memory.role === 'builder') {
+            roleBuilder.run(creep);
         }
     }
 }
