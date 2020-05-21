9c93cda917a6a2c1334428ef4dd68785f987ec4d
Alex <7alex7li@gmail.com>
2020-05-21 19:20:40 -0400

    .

--- main.js
+++ main.js
@@ -6,6 +6,22 @@ module.exports.loop = function () {
     let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === 'harvester');
     console.log('Harvesters: ' + harvesters.length);
 
+    if(harvesters.length < 2) {
+        let newName = 'Harvester' + Game.time;
+        console.log('Spawning new harvester: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
+            {memory: {role: 'harvester'}});
+    }
+
+    if(Game.spawns['Spawn1'].spawning) {
+        let spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+        Game.spawns['Spawn1'].room.visual.text(
+            '🛠️' + spawningCreep.memory.role,
+            Game.spawns['Spawn1'].pos.x + 1,
+            Game.spawns['Spawn1'].pos.y,
+            {align: 'left', opacity: 0.8});
+    }
+
     for (let name in Game.creeps) {
         let creep = Game.creeps[name];
         if (creep.memory.role === 'harvester') {
