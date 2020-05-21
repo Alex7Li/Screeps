355284805482bb3716acc031c7841e79a4441933
Alex <7alex7li@gmail.com>
2020-05-20 22:34:16 -0400

    Update main for upgrader

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-var roleHarvester = require('role.harvester');
+let roleHarvester = require('role.harvester');
+let roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
     for(let name in Game.creeps) {
         let creep = Game.creeps[name];
-        roleHarvester.run(creep);
+        if(creep.memory.role === 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role === 'upgrader') {
+            roleUpgrader.run(creep);
+        }
     }
 }
